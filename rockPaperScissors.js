dce735d8b36941582d2913fa5d37a3bfbf48447d
alecvohnoutka <alec.vohnoutka@gmail.com>
2023-01-11 19:11:28 -0500

    Add function to compare user and computer input.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,13 @@
 let computerChoiceBase =  Math.floor(Math.random() * 100) + 1;
+let userRPS = prompt("Rock, paper, or scissors?");
+let userChoice = userRPS.toLowerCase();
+let computerChoice;
+
+function getUserChoice() {
+    if (userRPS != null) {
+        return userChoice;
+    }
+}
 
 function getComputerChoice() {
     let computerChoice;
@@ -10,15 +19,33 @@ function getComputerChoice() {
     }
     else {computerChoice = "scissors";
     }
-    return computerChoice
+    return computerChoice;
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Rock, paper, or scissors?");
-    let result = userChoice.toLowerCase();
-    if (userChoice != null) {
-        return result;
+function playRound(){
+    let resultText;
+    if (userChoice === "rock" && getComputerChoice() === "paper") {
+        resultText = "You lose! Paper covers rock.";
     }
+    else if (userChoice === "rock" && getComputerChoice() === "scissors") {
+        resultText = "You win! Rock beats scissors.";
+    }
+    else if (userChoice === "paper" && getComputerChoice() === "rock") {
+        resultText = "You win! Paper covers rock.";
+    }
+    else if (userChoice === "paper" && getComputerChoice() === "scissors") {
+        resultText = "You lose! Scissors cuts paper.";
+    }
+    else if (userChoice === "scissors" && getComputerChoice() === "rock") {
+        resultText = "You lose! Rock beats scissors.";
+    }
+    else if (userChoice === "scissors" && getComputerChoice() === "paper") {
+        resultText = "You win! Scissors cuts paper.";
+    }
+    else {userChoice === getComputerChoice();
+        resultText = "It's a tie!";
+    }
+    return resultText;
 }
 
-console.log(getUserChoice())
+console.log(playRound())
